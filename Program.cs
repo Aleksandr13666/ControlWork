62d909854b0610ec563bceb9ff2b5c13b5197d25
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-02-24 17:48:38 +0700

    Create method Fills the array

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу которая из массива строк формирует новый массива
+// для строк которого меньше или равна 3 символа.
+
+
+Console.WriteLine("Input quantity of string: ");
+int n  = Convert.ToInt32(Console.ReadLine());
+
+
+string[] InputString(int n)
+{
+string[] str = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Input a string {0}:\r\n ", i + 1);
+    str[i] = Console.ReadLine();
+}
+
+return str;
+}
